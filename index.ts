8017f27bd9264e09f485a48dc82f5ba26779d0a5
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2026-02-03 20:26:25 +0700

    3.1. Tạo routes

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import { connect } from "./config/database";
-import Topic from "./models/topics.model";
 import dotenv from "dotenv";
+import clientRoutes from "./routes/client/index.route";
 
 dotenv.config();
 
@@ -13,15 +13,7 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/topics", async (req: Request, res: Response) => {
-  const topics = await Topic.find({
-    deleted: false,
-  });
-
-  console.log(topics);
-
-  res.render("client/pages/topics/index");
-});
+clientRoutes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
