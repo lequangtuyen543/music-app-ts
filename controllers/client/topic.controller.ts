e2f8cc0a9bf6db2370ecb9d0a4373e03c2ccfabb
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2026-02-07 18:56:28 +0700

    4. Làm trang Chủ đề bài hát (bên client)

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -7,9 +7,8 @@ export const topics = async (req: Request, res: Response) => {
     deleted: false,
   });
 
-  // console.log(topics);
-
   res.render("client/pages/topics/index", {
     pageTitle: "Chủ đề bài hát",
+    topics: topics,
   });
 };
