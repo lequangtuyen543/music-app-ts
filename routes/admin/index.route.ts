896905f86f52fa096f878794361b98157f068e80
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2026-02-27 20:02:01 +0700

    3. Trang Quản lý chủ đề (bên admin)

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,11 +1,14 @@
 import { Express } from "express";
 import { dashBoardRoutes } from "./dashboard.route";
 import { systemConfig } from "../../config/config";
+import { topicRoutes } from "./topic.route";
 
 const adminRoutes = (app: Express): void => {
   const PATH_ADMIN = systemConfig.PREFIX_ADMIN;
 
   app.use(`${PATH_ADMIN}/dashboard`, dashBoardRoutes);
+
+  app.use(`${PATH_ADMIN}/topics`, topicRoutes);
 };
 
 export default adminRoutes;
