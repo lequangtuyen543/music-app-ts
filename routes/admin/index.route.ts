99bcc3c00a846afef3a365289419b31bd5c93b21
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2026-02-27 20:17:52 +0700

    4. Tạo trang Danh sách bài hát (bên admin)

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import { dashBoardRoutes } from "./dashboard.route";
 import { systemConfig } from "../../config/config";
 import { topicRoutes } from "./topic.route";
+import { songRoutes } from "./song.route";
 
 const adminRoutes = (app: Express): void => {
   const PATH_ADMIN = systemConfig.PREFIX_ADMIN;
@@ -9,6 +10,8 @@ const adminRoutes = (app: Express): void => {
   app.use(`${PATH_ADMIN}/dashboard`, dashBoardRoutes);
 
   app.use(`${PATH_ADMIN}/topics`, topicRoutes);
+
+  app.use(`${PATH_ADMIN}/songs`, songRoutes);
 };
 
 export default adminRoutes;
