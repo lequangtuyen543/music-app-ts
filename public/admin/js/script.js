474552120c7a8b6f360e13234357e072b972b4f8
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2026-02-28 16:29:25 +0700

    5.1. Làm giao diện

---
+++ public/admin/js/script.js
@@ -0,0 +1,17 @@
+// Upload Image
+const uploadImage = document.querySelector("[upload-image]");
+
+if (uploadImage) {
+  const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+  const uploadImagePreview = uploadImage.querySelector(
+    "[upload-image-preview]"
+  );
+
+  uploadImageInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
+// End Upload Image
