37d0eac61da38da69202dd91027f40f79db17743
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2026-03-02 17:01:27 +0700

    1.2. Tính năng nghe thử audio

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -16,4 +16,21 @@ if (uploadImage) {
 }
 // End Upload Image
 
-console.log("ok");
+// Upload Audio
+const uploadAudio = document.querySelector("[upload-audio]");
+
+if (uploadAudio) {
+  const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+  const uploadAudioPlay = uploadAudio.querySelector("[upload-audio-play]");
+  const source = uploadAudio.querySelector("source");
+
+  uploadAudioInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      source.src = URL.createObjectURL(file);
+
+      uploadAudioPlay.load();
+    }
+  });
+}
+// End Upload Audio
